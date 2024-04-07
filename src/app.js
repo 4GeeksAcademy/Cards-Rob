e5dfd18d6ac73c51ffc2b9f6fa0ffcb0b6f9f9a3
Robert <155775143+Roberttovarv@users.noreply.github.com>
2024-04-07 14:39:33 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+
+  const showRandomCard = () => {
+
+    let icons = ["♠", "♥", "♣", "♦"];
+    let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+
+    const randomCardValue = array => {
+      return array[Math.floor(Math.random() * array.length)]; 
+    };
+
+    const randomIcon = randomCardValue(icons); // Generar un único icono aleatorio
+
+    const cardRandomNumber = document.querySelector('#middleCard');
+    cardRandomNumber.innerHTML = randomCardValue(values);
+
+    const cardRandomIcons = document.querySelectorAll(".cardIcon");
+    cardRandomIcons.forEach(icon => {
+      let iconValue = randomIcon; 
+      icon.innerHTML = iconValue;
+
+
+      if (iconValue === "♥" || iconValue === "♦") {
+        icon.classList.add("text-body-danger");
+      } else {
+        icon.classList.remove("text-body-danger");
+      }
+    });
+
+  };
+
+  showRandomCard();
+
 };
